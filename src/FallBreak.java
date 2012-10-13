f7580d2f28a2d0b6a747960fe21fb7040743fde0
rreinke <rreinke@mymail.mines.edu>
2012-10-13 15:28:13 -0600

    Adding int daysOff to Fall break class

--- src/FallBreak.java
+++ src/FallBreak.java
@@ -1,11 +1,19 @@
+import java.util.Scanner;
+
 
 public class FallBreak {
+	Scanner in = new Scanner(System.in);
 	
 	private String fallBreak;
+	private int daysOff;
 
 	public FallBreak() {
 		// TODO Auto-generated constructor stub
-		fallBreak = "Couldn't Agree More!";
+		System.out.println("How many days of break do you have?");
+		daysOff = in.nextInt();
+		fallBreak = "There are " + daysOff + " days of break left!";
+		
+		
 	}
 
 	public String toString() {
